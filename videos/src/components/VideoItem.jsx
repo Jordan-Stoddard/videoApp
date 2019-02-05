5d26aab13b512b9ba0f4fc2b3e73652d6dbe41c7
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-05 09:29:20 -0800

    Individual video component

--- videos/src/components/VideoItem.jsx
+++ videos/src/components/VideoItem.jsx
@@ -1,17 +1,19 @@
-import './VideoItem.css'
-import React from 'react'
+import "./VideoItem.css";
+import React from "react";
 
-const VideoItem = ({video, onVideoSelect}) => {
-    return (
-        <div onClick={() => onVideoSelect(video)} className="video-item item">
-        <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
-        <div className="content">
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.description}
+      />
+      <div className="content">
         <div className="header">{video.snippet.title}</div>
-        </div>
-        
-        
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default VideoItem
+export default VideoItem;
